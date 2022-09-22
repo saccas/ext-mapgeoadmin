23c6069f34cc6fa8e9a83d3eb5abcf5b5432dade
Daniel Siepmann <coding@daniel-siepmann.de>
2022-09-22 09:10:09 +0200

    Prepare v11 compatible release

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,26 +5,24 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Map geo admin iframe integration',
     'category' => 'misc',
     'shy' => 0,
-    'version' => '2.0.1',
+    'version' => '3.0.0',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
     'author' => '',
     'author_email' => '',
     'author_company' => '',
-    'autoload' =>
-        [
-            'psr-4' => ['Saccas\\Mapgeoadmin\\' => 'Classes']
-        ],
+    'autoload' => [
+        'psr-4' => [
+            'Saccas\\Mapgeoadmin\\' => 'Classes',
+        ]
+    ],
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.13-10.9.99',
+            'typo3' => '11.5.16-11.5.99',
         ],
         'conflicts' => [
         ],
         'suggests' => [
         ],
-    ],
-    '_md5_values_when_last_written' => 'a:0:{}',
-    'suggests' => [
     ],
 ];
