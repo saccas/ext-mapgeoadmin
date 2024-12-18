fe2388bbd2c4018008f90dbab3e0a3812a3eecdc
Christian Wolfram <c.wolfram@chriwo.de>
2024-12-18 11:05:25 +0100

    [TASK] Add TYPO3 Rector

--- Classes/Controller/IframeController.php
+++ Classes/Controller/IframeController.php
@@ -2,17 +2,18 @@
 
 namespace Saccas\Mapgeoadmin\Controller;
 
+use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class IframeController extends ActionController
 {
     protected $iframeEmbedUrl = 'https://map.geo.admin.ch/embed.html';
 
-    public function indexAction(): void
+    public function indexAction(): ResponseInterface
     {
         $tsfe = $this->getTsfe();
 
-        $urlParams = (parse_url($this->settings['mapgeoadmin']['url'], PHP_URL_QUERY));
+        $urlParams = (parse_url((string)$this->settings['mapgeoadmin']['url'], PHP_URL_QUERY));
 
         $iframeLinkConf = [
             'parameter' => $this->iframeEmbedUrl . '?' . $urlParams,
@@ -20,6 +21,8 @@ class IframeController extends ActionController
 
         $iframeSrc = $tsfe->cObj->typoLink_URL($iframeLinkConf);
         $this->view->assign('iframeSrc', $iframeSrc);
+
+        return $this->htmlResponse();
     }
 
     private function getTsfe()
--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,17 +1,23 @@
 <?php
 
-defined('TYPO3_MODE') or die();
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
+defined('TYPO3') or die();
 
 (static function ($extensionKey) {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-        'Saccas.' . $extensionKey,
+    ExtensionUtility::registerPlugin(
+        'Mapgeoadmin',
         'Embed',
         'Map geo admin / iframe',
         'EXT:mapgeoadmin/Resources/Public/Icons/Extension.png'
     );
 
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$extensionKey . '_embed'] = 'recursive,select_key,pages';
-
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$extensionKey . '_embed'] = 'pi_flexform';
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($extensionKey . '_embed', 'FILE:EXT:' . $extensionKey . '/Configuration/FlexForms/MapgeoadminIframe.xml');
+
+    ExtensionManagementUtility::addPiFlexFormValue(
+        $extensionKey . '_embed',
+        'FILE:EXT:' . $extensionKey . '/Configuration/FlexForms/MapgeoadminIframe.xml'
+    );
 })('mapgeoadmin');
